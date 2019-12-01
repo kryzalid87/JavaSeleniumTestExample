88c49dc927e529f8ab07a31f7c1e015566902534
Paweł Jakubik <dev@goalscheck.com>
2019-12-01 14:51:33 +0100

    Unify remoteHub property

--- src/main/java/Helper/TestData/SystemData.java
+++ src/main/java/Helper/TestData/SystemData.java
@@ -6,7 +6,7 @@ public class SystemData {
      * @return system property hubUrl
      */
     public static String getHubAddress(){
-        return System.getProperty("hubUrl");
+        return System.getProperty("remoteHub");
     }
 
     /***
